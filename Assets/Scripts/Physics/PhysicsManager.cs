e294b25e24262a87b2a6b0e5e4a2a6549cf77516
BRackhamUP <bradrack@icloud.com>
2025-10-23 16:33:41 +0100

    Update PhysicsManager.cs
    Attempting to redesign the gravity application from a manager object into a static method that will be applied in fixedUpdate() ... cannot check for syntax errors, trying to do this on my phone (curse public transport)

--- Assets/Scripts/Physics/PhysicsManager.cs
+++ Assets/Scripts/Physics/PhysicsManager.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PhysicsManager : MonoBehaviour
+public static class PhysicsManager
 {
 
     /*
@@ -21,30 +21,13 @@ public class PhysicsManager : MonoBehaviour
 
      */
 
-    [SerializeField] public Vector3 globalGravity = new Vector3(0, -9.81f, 0);
-    [SerializeField] private List<Rigidbody> physicsObjects = new List<Rigidbody>();
+    public Vector3 globalGravity = new Vector3(0, -9.81f, 0);
 
-    public void RegisterRigidbody(Rigidbody rb)
+    public static void ApplyGravity(Transform obj)
     {
-        if (!physicsObjects.Contains(rb))
-        {
-            physicsObjects.Add(rb);
-        }
-    }
-
-    void FixedUpdate()
-    {
-        foreach (Rigidbody rb in physicsObjects)
-        {
-            // Gravity
-            rb.AddForce(globalGravity, ForceMode.Acceleration);
-
-            // AirDrag
-            PhysicsObject po = rb.GetComponent<PhysicsObject>();
-            if (po != null) 
-            {
-                po.ApplyingAirDrag(rb);
-            }
-        }
+        Rigidbody objRb = obj.GetComponent<Rigidbody>();
+         // Gravity
+         objRb.AddForce(globalGravity, ForceMode.Acceleration);
+            
     }
 }
